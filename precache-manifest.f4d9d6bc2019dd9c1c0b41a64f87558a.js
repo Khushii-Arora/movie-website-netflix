6488b340e586c0582b7728f7e73212cbd54ca1f4
Khushi Arora <akhushi72@gmail.com>
2023-07-28 17:46:51 +0530

    Updates

---
+++ precache-manifest.f4d9d6bc2019dd9c1c0b41a64f87558a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbad4862e3e416c2fd3b7a1f957b99f",
+    "url": "/movie-website-netflix/index.html"
+  },
+  {
+    "revision": "9ccdf34dfcc5d050ab6f",
+    "url": "/movie-website-netflix/static/css/main.23c9dd4a.chunk.css"
+  },
+  {
+    "revision": "31e52e9769102ad6d6b3",
+    "url": "/movie-website-netflix/static/js/2.015d6aa0.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/movie-website-netflix/static/js/2.015d6aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccdf34dfcc5d050ab6f",
+    "url": "/movie-website-netflix/static/js/main.257f2100.chunk.js"
+  },
+  {
+    "revision": "3a4086ec96fea73a27c2",
+    "url": "/movie-website-netflix/static/js/runtime-main.6afc0a23.js"
+  }
+]);
